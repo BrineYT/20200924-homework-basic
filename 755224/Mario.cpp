1c5f7f3cbafc12d6aac1d89856099e2a225fba37
BrineYT <60344483+BrineYT@users.noreply.github.com>
2020-09-28 21:38:10 +0800

    Update Mario.cpp

--- 755224/Mario.cpp
+++ 755224/Mario.cpp
@@ -5,11 +5,11 @@ int main() {
     int layers;
     cout << "The amount of layers: ";
     cin >> layers;
-    for(int i = layers; i > 0; i--) {
-      for(int j = i; j > 0; j--) {
+    for (int i = layers; i > 0; i--) {
+      for (int j = i; j > 0; j--) {
         cout << " ";
       }
-      for(int k = layers - i + 1; k > 0; k--) {
+      for (int k = layers - i + 1; k > 0; k--) {
         cout << "#";
       }
       cout << "\n";
